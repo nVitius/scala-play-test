9e59c26a9124444e997ad2b7de647a68eb42fd03
nVitius <mauricio@nviti.us>
2017-07-10 13:56:42 -0700

    Remove sample UI files

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,8 +1,15 @@
 package controllers
 
 import javax.inject._
+
+import java.net.UnknownHostException
+import akka.http.scaladsl.model.headers.CacheDirectives.public
+import com.fasterxml.jackson.databind.JsonNode
 import play.api._
 import play.api.mvc._
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import org.jsoup.HttpStatusException
+import play.api.libs.json.{JsValue, Json}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -19,6 +26,18 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    Ok(Json.obj("status" -> 200))
   }
+
+  def fetchTitle() = Action(parse.json) { request =>
+    val browser = JsoupBrowser()
+    try {
+      val doc = browser.get((request.body \ "site").as[String])
+      Ok(Json.obj("status" -> 200, "title" -> doc.title))
+    } catch {
+      case unkHostE: UnknownHostException => BadRequest(Json.obj("status" -> 400, "message" -> "Invalid host"))
+      case httpE: HttpStatusException => BadRequest(Json.obj("status" -> 400, "message" -> "Error scraping page"))
+    }
+  }
+
 }
